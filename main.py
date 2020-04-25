7f81aea8ddbfae5c2fe50cc1c9c675a5a81fe728
hellocomrade <sun8726@hotmail.com>
2020-04-25 20:12:43 +0000

    Downstream to MQ

--- main.py
+++ main.py
@@ -1,27 +1,40 @@
 import sys
 import time
+import pika
 
 from multiprocessing import Process, Queue, Pool, Manager
 
 
-def worker(idx, queue):
+def worker(idx, queue, message_queue_host, message_queue_exchange):
     print(f'Worker {idx} is up')
     while True:
         msg = queue.get()
-        print(f'Worker {idx}: {msg}')
-        time.sleep(1)
+        # print(f'Worker {idx}: {msg}')
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host=message_queue_host))
+        channel = connection.channel()
+        channel.basic_publish(exchange=message_queue_exchange, routing_key='shock-index', body=msg)
+        # connection.close()
+
+
+def listener(dpath, q):
+    with open(dpath, 'r') as f:
+        for line in f:
+            q.put(line)
 
 
 if '__main__' == __name__:
-    cores = max(1, 2 if 1 == len(sys.argv) else int(sys.argv[1]))
+    if 5 > len(sys.argv):
+        print(f'{__file__} number_of_worker, message_queue_host, message_queue_exchange_name, data_file_path', file=sys.stderr)
+        sys.exit()
+    cores, mq_host, mq_exchange, path = max(1, int(sys.argv[1])), str(sys.argv[2]), str(sys.argv[3]), str(sys.argv[4])
     mgr = Manager()
     q = mgr.Queue()
     pool = Pool(cores)
-    workers = [pool.apply_async(worker, (i, q)) for i in range(cores)]
-    for i in range(20):
-        q.put(2**i)
+    workers = [pool.apply_async(worker, (i, q, mq_host, mq_exchange)) for i in range(cores)]
+    Process(target=listener, args=(path, q)).start()
+
     try:
         [worker.get() for worker in workers]
-    except:
+    except Exception as e:
         pool.terminate()
         pool.join()
