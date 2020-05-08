390b29ed9fe6c8755119b9b09234f83fb6ec4077
hellocomrade <sun8726@hotmail.com>
2020-05-08 22:19:56 +0000

    Created 10 queues for pressure testing

--- task1/celeryconfig.py
+++ task1/celeryconfig.py
@@ -2,8 +2,8 @@
 # The machinery in charge of routing messages.
 broker_url = 'pyamqp://guest:guest@192.168.56.102:5672/'
 message_queue_host = '192.168.56.102'
-message_queue_exchange = 'shock-index'
-message_queue_routing_key = 'shock-index'
+message_queue_exchange = 'waveform-exchange'
+message_queue_routing_key = ['q1', 'q2', 'q3', 'q4', 'q5', 'q6', 'q7', 'q8', 'q9', 'q10']
 # Also use Redis to store results.
 # result_backend = 'redis://'
 
--- task1/worker.py
+++ task1/worker.py
@@ -9,7 +9,6 @@ def send(self, msg):
     from . import channel
     obj = json.loads(msg)
     if 'wav' in obj:
-        for wav in obj['wav']:
-            if '1814' == wav['id']:
-                channel[0].basic_publish(exchange=celeryconfig.message_queue_exchange, routing_key=celeryconfig.message_queue_routing_key, body=msg)
-                break
+        for key in celeryconfig.message_queue_routing_key:
+            channel[0].basic_publish(exchange=celeryconfig.message_queue_exchange, routing_key=key, body=msg)
+
